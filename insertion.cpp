4579938586279069d74e42e076acddd5abdcf0fe
rifkaaikaa07-arc <rifkaaikaa07@gmail.com>
2026-03-10 11:25:05 +0700

    membuat prosedur data

--- insertion.cpp
+++ insertion.cpp
@@ -4,5 +4,18 @@ using namespace std;
 int arr [20];       // membuat array dengan panjang data 20
 int n;              // membuat variable inputan n
 
+void InputData (){
+    while (true){
+        cout << "Masukkan Jumlah Data Pada Array : ";
+        cin >> n;
+
+        if (n <= 20){
+            break;
+        }
+        else {
+            cout << "\nArray Tidak Boleh Lebih Dari 20\n";
+        }
+    }
+}
 int main (){   
 }
